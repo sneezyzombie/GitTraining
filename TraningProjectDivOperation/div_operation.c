fadf95da651a3505875a40bd46022af5e5d7a9bb
yellowscientist <reddan1470@gmail.com>
2022-10-01 22:24:37 +0300

    add

--- TraningProjectDivOperation/div_operation.c
+++ TraningProjectDivOperation/div_operation.c
@@ -3,11 +3,13 @@
 int main() {
 	int a, b;
 	float div;
+
+	printf("input dividend and dividor: a b\n");
 	scanf_s("%d %d", &a, &b);
 
 	if (b != 0) {
 		div = a / b;
-		printf("%d\n", div);
+		printf("%d / %d = %.2f\n",a, b, div);
 	}
 	else
 	{
